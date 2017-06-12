e9bbc9d4a5d6b78799c5f6e1542fe249f21aa81a
Marco Steybe <sty@zuehlke.com>
2017-06-12 15:33:42 +0200

    dummy parties page

---
+++ src/pages/parties/parties.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {PartiesPage} from './parties';
+
+@NgModule({
+  declarations: [
+    PartiesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PartiesPage),
+  ],
+  exports: [
+    PartiesPage
+  ]
+})
+export class PartiesPageModule {
+}
---
+++ src/pages/parties/parties.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PartiesPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-parties',
+  templateUrl: 'parties.html',
+})
+export class PartiesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PartiesPage');
+  }
+
+}
